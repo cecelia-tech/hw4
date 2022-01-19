b2aadf3be1646e415b3722cc5ffba9f59dc58a4e
vita <vitagriciute@live.com>
2022-01-19 12:08:28 +0200

    Initial check-in of module Homework4

---
+++ Task1/DiagonalMatrix.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace Task1
+{
+    public class DiagonalMatrix<T>
+    {
+        internal T[] DiagonalNumbers { get; }
+        public int Size { get; }
+
+        public DiagonalMatrix(int size, params T[] diagonalNumbers)
+        {
+            if (size < 0 || size > diagonalNumbers.Length) //might not need check for bigger than lenght
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                Size = size;
+            }
+
+            this.DiagonalNumbers = diagonalNumbers;
+        }
+
+        public T this[int i, int j]
+        {
+            get
+            {
+                if (i < 0 ||
+                    j < 0 ||
+                    i >= Size ||
+                    j >= Size)
+                {
+                    throw new IndexOutOfRangeException();
+                } 
+                else if (i == j)
+                {
+                    return DiagonalNumbers[i];
+                }
+                else
+                {
+                    return default;
+                }
+            }
+            set
+            {
+                if (i < 0 ||
+                    j < 0 ||
+                    i >= Size ||
+                    j >= Size)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                else if (i == j)
+                {
+                    DiagonalNumbers[i] = value;
+                }
+            }
+        }
+    }
+}
